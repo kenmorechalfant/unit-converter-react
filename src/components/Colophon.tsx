e4a275537264c016e0b2829f918b91e87d26ca77
Kenmore Chalfant <kenmorechalfant@gmail.com>
2022-01-20 21:38:39 -0700

    Improved styles to be more responsive. Keeps aspect ratio, scales down to fit vertically.

--- src/components/Colophon.tsx
+++ src/components/Colophon.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 function Colophon() {
   return (
     <div className="Colophon">
-      <a href="https://lukechalfant.com/">Home</a>
+      <a href="https://lukechalfant.com/">Luke Chalfant</a>
       <a href="https://www.linkedin.com/in/kenmorechalfant/">LinkedIn</a>
-      <a href="https://github.com/kenmorechalfant">View the Source on Github</a>
+      <a href="https://github.com/kenmorechalfant/unit-converter-react">Github</a>
     </div>
   );
 }

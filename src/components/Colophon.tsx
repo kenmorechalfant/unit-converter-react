cb11ad9ed8bf01fbdb7671ed9e225b24415ccd1d
Kenmore Chalfant <kenmorechalfant@gmail.com>
2022-01-22 10:14:49 -0700

    Added icons for Linkedin and Github in Colophon.

--- src/components/Colophon.tsx
+++ src/components/Colophon.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BsLinkedin as IconLinkedin, BsGithub as IconGithub } from 'react-icons/bs';
 
 function Colophon() {
   return (
     <div className="Colophon">
-      <a href="https://lukechalfant.com/">Luke Chalfant</a>
-      <a href="https://www.linkedin.com/in/kenmorechalfant/">LinkedIn</a>
-      <a href="https://github.com/kenmorechalfant/unit-converter-react">Github</a>
+      <a href="https://lukechalfant.com/" aria-label="Luke's Website">Created by Luke Chalfant</a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Linked-in" href="https://www.linkedin.com/in/kenmorechalfant/"><IconLinkedin /></a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Github" href="https://github.com/kenmorechalfant/unit-converter-react"><IconGithub /></a>
     </div>
   );
 }

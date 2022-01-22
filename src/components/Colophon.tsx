9eaf0d074ce4c58d7efed5740002b3a59a177fd2
Kenmore Chalfant <kenmorechalfant@gmail.com>
2022-01-22 15:24:19 -0700

    Updating Colophon link titles

--- src/components/Colophon.tsx
+++ src/components/Colophon.tsx
@@ -4,9 +4,9 @@ import { BsLinkedin as IconLinkedin, BsGithub as IconGithub } from 'react-icons/
 function Colophon() {
   return (
     <div className="Colophon">
-      <a href="https://lukechalfant.com/" aria-label="Luke's Website">Created by Luke Chalfant</a>
-      <a className="Colophon__Link--IconLink" aria-label="Luke's Linked-in" href="https://www.linkedin.com/in/kenmorechalfant/"><IconLinkedin /></a>
-      <a className="Colophon__Link--IconLink" aria-label="Luke's Github" href="https://github.com/kenmorechalfant/unit-converter-react"><IconGithub /></a>
+      <a href="https://lukechalfant.com/" aria-label="Luke's Website" title="lukechalfant.com">Created by Luke Chalfant</a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Linked-in" title="Luke's Linkedin (New Tab)" target="_blank" href="https://www.linkedin.com/in/kenmorechalfant/"><IconLinkedin /></a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Github" title="Github Repo for this Project (New Tab)" target="_blank" href="https://github.com/kenmorechalfant/unit-converter-react"><IconGithub /></a>
     </div>
   );
 }

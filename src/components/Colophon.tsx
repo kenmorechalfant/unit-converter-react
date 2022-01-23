37a3b3cfd777ed7fd593fff44daf5d8b35d7187f
Kenmore Chalfant <kenmorechalfant@gmail.com>
2022-01-23 09:03:39 -0700

    Lighthouse: external rel noopener and tweaking styles.

--- src/components/Colophon.tsx
+++ src/components/Colophon.tsx
@@ -4,9 +4,9 @@ import { BsLinkedin as IconLinkedin, BsGithub as IconGithub } from 'react-icons/
 function Colophon() {
   return (
     <div className="Colophon">
-      <a href="https://lukechalfant.com/" aria-label="Luke's Website" title="lukechalfant.com">Created by Luke Chalfant</a>
-      <a className="Colophon__Link--IconLink" aria-label="Luke's Linked-in" title="Luke's Linkedin (New Tab)" target="_blank" href="https://www.linkedin.com/in/kenmorechalfant/"><IconLinkedin /></a>
-      <a className="Colophon__Link--IconLink" aria-label="Luke's Github" title="Github Repo for this Project (New Tab)" target="_blank" href="https://github.com/kenmorechalfant/unit-converter-react"><IconGithub /></a>
+      <a href="https://lukechalfant.com/" aria-label="Luke's Website" title="lukechalfant.com"><span className="u-font-weight--normal">Made by</span> Luke Chalfant</a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Linked-in" title="Luke's Linkedin (New Tab)" target="_blank" rel="noopener" href="https://www.linkedin.com/in/kenmorechalfant/"><IconLinkedin /></a>
+      <a className="Colophon__Link--IconLink" aria-label="Luke's Github" title="Github Repo for this Project (New Tab)" target="_blank" rel="noopener" href="https://github.com/kenmorechalfant/unit-converter-react"><IconGithub /></a>
     </div>
   );
 }
